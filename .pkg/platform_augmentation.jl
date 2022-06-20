902ff4b770c4a3c8ed0b996bf1c103c7a18fad53
jlbuild <juliabuildbot@gmail.com>
2022-06-20 11:30:13 +0000

    SPIRV_LLVM_Translator_unified_jll build 0.2.0+0

--- .pkg/platform_augmentation.jl
+++ .pkg/platform_augmentation.jl
@@ -1,7 +1,7 @@
 using Base.BinaryPlatforms
 
     function augment_llvm!(platform::Platform)
-        haskey(platform, "llvm_version") && return p
+        haskey(platform, "llvm_version") && return platform
 
         llvm_version = Base.libllvm_version
         # does our LLVM build use assertions?
@@ -23,4 +23,3 @@ using Base.BinaryPlatforms
 function augment_platform!(platform::Platform)
     augment_llvm!(platform)
 end
-
